6a027ea901b45b4df1d80a207206006fe66d8f51
Renzo <renzoboyonas060606@gmail.com>
2026-04-26 17:34:56 +0800

    Auto-link ticket when creating work order from ticket view

--- modules/workorders/add.php
+++ modules/workorders/add.php
@@ -9,6 +9,10 @@ require_once __DIR__ . '/_styles.php';
 
 $is_edit     = false;
 $wo          = [];
+$ticket_id   = (int)($_GET['ticket_id'] ?? 0);
+if ($ticket_id > 0) {
+    $wo['ticket_id'] = $ticket_id;
+}
 $errors      = $_SESSION['wo_errors'] ?? [];
 $old         = $_SESSION['wo_old'] ?? [];
 unset($_SESSION['wo_errors'], $_SESSION['wo_old']);
